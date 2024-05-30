6fa8812b6967b2ecc64fa92231ca6476977832d7
MioGitAccount <momastojanovicpn@gmail.com>
2024-05-30 23:27:09 +0200

    Image media type adjusted
    MediaType set as "image/jpeg" for now

--- SpringServer/src/main/java/com/example/demo/controllers/BookController.java
+++ SpringServer/src/main/java/com/example/demo/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -75,7 +76,8 @@ public class BookController {
     @ResponseBody
     public ResponseEntity<Resource> viewImage(@PathVariable String filename){
         Resource resource = service.getImage(filename);
-        return ResponseEntity.ok()
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/jpeg"))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
